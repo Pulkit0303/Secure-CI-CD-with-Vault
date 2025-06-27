19cdfb047bec1d6c46ffbc3fbc44630221d38c7e
Pulkit1903 <pulkit.19.2003@gmail.com>
2025-06-27 18:25:50 +0530

    Jenkins Installation file added

--- Infrastructure/ec2.tf
+++ Infrastructure/ec2.tf
@@ -1,8 +1,49 @@
-//EC2 Instance
+resource "aws_security_group" "my_sg" {
+  name        = "jenkins-vault-sg"
+  description = "Allow Jenkins, Vault, and SSH ports"
+
+  ingress {
+    description = "Allow Jenkins (8080)"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Vault UI/API (8200)"
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-vault-sg"
+  }
+}
+
 resource "aws_instance" "ec2_inst" {
-  ami = var.my_ami
+  ami           = var.my_ami
   instance_type = var.my_type
-  key_name = ""
+  user_data     = file("${path.module}/jenkins-install.sh")
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+
   tags = {
     Name = var.my_name
   }
